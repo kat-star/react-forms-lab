e9a9e2dbf8c3769f02218161593545500d21ac68
kat-star <katherinewkwong@gmail.com>
2019-11-19 21:19:11 -0800

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,49 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: '',
+      password: ''
+    };
   }
+
+  handleLogin = event => {
+    event.preventDefault();
+    if (this.state.password && this.state.username) {
+      const loginData = {
+        username: this.state.username,
+        password: this.state.password
+      }
+      this.props.handleLogin(loginData); 
+    }
+  }
+
+  handleUsernameChange = event => (
+    this.setState({
+      username: event.target.value
+    })
+  );
+
+
+  handlePasswordChange = event => (
+    this.setState({
+      password: event.target.value
+    })
+  );
 
   render() {
     return (
-      <form>
+      <form onSubmit={event => this.handleLogin(event)}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleUsernameChange} id="username" name="username" type="text" value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handlePasswordChange} id="password" name="password" type="password" value={this.state.password} />
           </label>
         </div>
         <div>
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -3,18 +3,28 @@ import React from "react";
 class TwitterMessage extends React.Component {
   constructor() {
     super();
-
-    this.state = {};
+    this.state = {
+      message: ""
+    };
   }
+
+  handleMessageClick = event => {
+    this.setState({
+      message: event.target.value
+    })
+  }
+
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <div>Characters Remaining:{this.props.maxChars - this.state.message.length}</div>
+        <input onChange={event => this.handleMessageClick(event)} type="text" name="message" id="message" value={this.state.message} />
       </div>
     );
   }
+
 }
 
 export default TwitterMessage;
